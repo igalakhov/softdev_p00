17db7ee0fb6b6971aedd641644dcb5740e702fa6
Michael Lin <mlin01@stuy.edu>
2019-10-22 08:40:18 -0400

    fixed some stuff in story class

--- app/database/story.py
+++ app/database/story.py
@@ -2,13 +2,11 @@ class Story:
 
     # initialize story with id
     def __init__(self,id):
-        title = "title_here"
-        author = "author_here" #will be changed to a user object
-        time = "time_here"
-        first_addition = "id" #will be changed to a story_addition object
+        self.title = "title_here"
+        self.author = "author_here" #will be changed to a user object
+        self.time = "time_here"
+        self.first_addition = "id" #will be changed to a story_addition object
 
     # returns a list of story_addition object
     def get_additions(self):
         pass
-
-
