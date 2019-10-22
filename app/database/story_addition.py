5d45c5509271e916145e2f8b3eac92b3716849dd
jjshawshaw <jshaw00@stuy.edu>
2019-10-22 08:35:34 -0400

    wrote layout for empty story and story_addition classes

--- app/database/story_addition.py
+++ app/database/story_addition.py
@@ -1,4 +1,14 @@
 class StoryAddition:
 
-    def __init__(self):
+    # initialize addition with id
+    def __init__(self, id):
+        title = "title_here"
+        author = "author_here" #will be changed to a user object
+        time = "time_here"
+
+    # returns the next addition in the story 
+    def nextAddition(self):
         pass
+
+
+
