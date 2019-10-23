8166b4e5fcc657e5a4f61a76aa9700a1bb96d883
Ivan Galakhov <igalakhov00@stuy.edu>
2019-10-22 21:39:48 -0400

    work

--- app/database/story_addition.py
+++ app/database/story_addition.py
@@ -3,8 +3,12 @@ class StoryAddition:
     # initialize addition with id
     def __init__(self, id):
         self.title = "title_here"
-        self.author = "author_here" #will be changed to a user object
+        self.author = "author_here"  # will be changed to a user object
         self.time = "time_here"
 
+    # static methods
 
-
+    # creates a new story addition
+    @staticmethod
+    def new_story_addition(user, story, content):
+        pass
