41452f383715bff095b7b8eb1e9719a3ee19d391
Michael Lin <mlin01@stuy.edu>
2019-10-24 08:38:24 -0400

    added in story addition

--- app/database/story_addition.py
+++ app/database/story_addition.py
@@ -2,13 +2,19 @@ class StoryAddition:
 
     # initialize addition with id
     def __init__(self, id):
-        self.title = "title_here"
-        self.author = "author_here"  # will be changed to a user object
-        self.time = "time_here"
+        data = execute_command('SELECT * FROM `story_addition` WHERE `story_addition`.id=%d' % id).fetchall()
+        assert (len(data) != 0)  # no making non existing users!
+
+        self.id = data[0][0]
+        self.time_created = data[0][1]
+        self.content = data[0][2]
+        self.story_id = data[0][3]
+        self.author_id = data[0][4]
 
     # static methods
 
     # creates a new story addition
     @staticmethod
     def new_story_addition(user, story, content):
-        pass
+        execute_command('INSERT INTO `story_addition` (content, story.id,user.id)'
+                        'VALUES (\"%s\", \"%s\", \"%s\")' % (content, story.id, user.id))
